65b7e7133e486f05449f52b56e6bf0f5b1c8b228
Samuel Joshua <153777108+ksj047@users.noreply.github.com>
2024-11-07 15:27:44 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Add event listener to the contact link
-document.querySelector('footer a').addEventListener('click', function() {
-    // Open the link in a new tab
-    window.open(this.href, '_blank');
-});
